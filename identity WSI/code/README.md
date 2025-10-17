463308d645b3d101c1c609a7d7e928f9613ffa1d
sidhamed <siddiqhamed2017@gmail.com>
2025-10-17 10:35:05 +0200

    chore: remove WSI dummy task

--- identity WSI/code/README.md
+++
@@ -1,3 +0,0 @@
-# identity-WSI
-
-A minimal task for the App Engine that returns its WSI input as output.
--- identity WSI/code/scripts/identity.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-INPUT_FOLDER=/inputs
-OUTPUT_FOLDER=/outputs
-
-cp $INPUT_FOLDER/input $OUTPUT_FOLDER/output
